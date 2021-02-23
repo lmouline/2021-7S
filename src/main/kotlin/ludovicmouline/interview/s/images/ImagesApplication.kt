b54287db8ceab55bec54d596f15c3c1fde0cee52
Ludovic Mouline <ludovic.mouline@tutanota.com>
2021-02-23 15:45:07 +0100

    customise the error message sent for error handling

--- src/main/kotlin/ludovicmouline/interview/s/images/ImagesApplication.kt
+++ src/main/kotlin/ludovicmouline/interview/s/images/ImagesApplication.kt
@@ -2,9 +2,32 @@ package ludovicmouline.interview.s.images
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.boot.web.error.ErrorAttributeOptions
+import org.springframework.boot.web.servlet.error.DefaultErrorAttributes
+import org.springframework.stereotype.Component
+import org.springframework.web.context.request.WebRequest
 
 @SpringBootApplication
 class ImagesApplication
+
+
+
+@Component
+class ErrorMessage: DefaultErrorAttributes() {
+	companion object {
+		const val TRACE_KEY = "trace"
+	}
+
+	override fun getErrorAttributes(
+		webRequest: WebRequest?,
+		options: ErrorAttributeOptions?
+	): MutableMap<String, Any> {
+		val entries: MutableMap<String, Any> =
+			super.getErrorAttributes(webRequest, options)
+		entries.remove(TRACE_KEY)
+		return entries
+	}
+}
 
 fun main(args: Array<String>) {
 	runApplication<ImagesApplication>(*args)
