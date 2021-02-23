af6d9213d1d93412267ee1860304672d1d815fa8
Ludovic Mouline <ludovic.mouline@tutanota.com>
2021-02-23 12:02:47 +0100

    Init application from Spring Initializr

---
+++ src/main/kotlin/ludovicmouline/interview/s/images/ImagesApplication.kt
@@ -0,0 +1,11 @@
+package ludovicmouline.interview.s.images
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ImagesApplication
+
+fun main(args: Array<String>) {
+	runApplication<ImagesApplication>(*args)
+}
---
+++ src/test/kotlin/ludovicmouline/interview/s/images/ImagesApplicationTests.kt
@@ -0,0 +1,13 @@
+package ludovicmouline.interview.s.images
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ImagesApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
