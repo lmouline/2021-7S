07f628eba38c742def0b02c3cacb1274a18f09b0
Ludovic Mouline <ludovic.mouline@tutanota.com>
2021-02-23 23:10:38 +0100

    one remaining image in a function name

--- src/test/kotlin/ludovicmouline/interview/strips/StripControllerTest.kt
+++ src/test/kotlin/ludovicmouline/interview/strips/StripControllerTest.kt
@@ -9,15 +9,12 @@ import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
 
-
 @WebMvcTest(StripController::class)
-class StripControllerTest(
-    @Autowired val mockMvc: MockMvc
-) {
+class StripControllerTest(@Autowired val mockMvc: MockMvc) {
 
     @Test
     fun `Assert images return 20 images`(){
-        this.mockMvc.perform(get("/images"))
+        this.mockMvc.perform(get("/strips"))
             .andExpect(status().isOk)
             .andExpect(content().contentType(MediaType.APPLICATION_JSON))
             .andExpect(jsonPath("\$.length()").value(20))
