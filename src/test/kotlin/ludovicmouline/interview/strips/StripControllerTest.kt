12cbb3609994fe4b091cf1e1adc760d6a939c1b1
Ludovic Mouline <ludovic.mouline@tutanota.com>
2021-02-24 08:03:00 +0100

    fix test to check the content of the result

--- src/test/kotlin/ludovicmouline/interview/strips/StripControllerTest.kt
+++ src/test/kotlin/ludovicmouline/interview/strips/StripControllerTest.kt
@@ -37,12 +37,15 @@ class StripControllerTest(@Autowired val appCtx: WebApplicationContext,
         Mockito.`when`(xkcd.pullImages(10))
             .thenReturn(arrayListOf(
                 Strip(title = "XKCD 1", webUrl = "http://xkcd.com/1",
+                    description= "Description 1",
                     pictureUrl = "http://xkcd.com/1.png",
                     publishedDate = LocalDate.of(2015, 8, 21)),
                 Strip(title = "XKCD 2", webUrl = "http://xkcd.com/2",
+                    description= "Description 2",
                     pictureUrl = "http://xkcd.com/2.png",
                     publishedDate = LocalDate.of(2021, 1, 6)),
                 Strip(title = "XKCD 3", webUrl = "http://xkcd.com/3",
+                    description= "Description 3",
                     pictureUrl = "http://xkcd.com/3.png",
                     publishedDate = LocalDate.of(2020, 10, 5))
             ))
@@ -57,10 +60,10 @@ class StripControllerTest(@Autowired val appCtx: WebApplicationContext,
                 Strip(title = "PDL 2", webUrl = "http://pdl.com/2",
                     pictureUrl = "http://pdl.com/2.png",
                     publishedDate = LocalDate.of(2013, 7, 7)),
-                Strip(title = "PDL 3", webUrl = "http://PDL.com/3",
+                Strip(title = "PDL 3", webUrl = "http://pdl.com/3",
                     pictureUrl = "http://pdl.com/3.png",
                     publishedDate = LocalDate.of(2021, 10, 5)),
-                Strip(title = "PDL 4", webUrl = "http://PDL.com/4",
+                Strip(title = "PDL 4", webUrl = "http://pdl.com/4",
                     pictureUrl = "http://pdl.com/4.png",
                     publishedDate = LocalDate.of(2018, 10, 5))
             ))
@@ -78,7 +81,8 @@ class StripControllerTest(@Autowired val appCtx: WebApplicationContext,
             .andExpect(content().contentType("application/hal+json"))
             .andExpect(
                 jsonPath("\$._embedded.stripList[*].title").value(
-                    arrayListOf("PDL 3", "XKCD 2", "XKCD 3", "PDL 4", "XKCD 1", "PDL 2", "PDL 1")
+                    arrayListOf("PDL 3", "XKCD 2", "XKCD 3", "PDL 4", "XKCD 1",
+                        "PDL 2", "PDL 1")
                 )
             )
     }
@@ -98,20 +102,36 @@ class StripControllerTest(@Autowired val appCtx: WebApplicationContext,
         this.mockMvc.perform(get("/strips"))
             .andExpect(status().isOk)
             .andExpect(content().contentType("application/hal+json"))
+            .andDo { println(it.response.contentAsString) }
             .andExpect(
-                jsonPath("\$._embedded.stripList[*].title").exists()
+                jsonPath("\$._embedded.stripList[*].title").value(
+                    arrayListOf("PDL 3", "XKCD 2", "XKCD 3", "PDL 4", "XKCD 1",
+                        "PDL 2", "PDL 1")
+                )
             )
             .andExpect(
-                jsonPath("\$._embedded.stripList[*].description").exists()
+                jsonPath("\$._embedded.stripList[*].description").value(
+                    arrayListOf("", "Description 2", "Description 3", "",
+                        "Description 1", "", "")
+                )
             )
             .andExpect(
-                jsonPath("\$._embedded.stripList[*].webUrl").exists()
+                jsonPath("\$._embedded.stripList[*].webUrl").value(
+                    arrayListOf("http://pdl.com/3", "http://xkcd.com/2", "http://xkcd.com/3", "http://pdl.com/4", "http://xkcd.com/1",
+                        "http://pdl.com/2", "http://pdl.com/1")
+                )
             )
             .andExpect(
-                jsonPath("\$._embedded.stripList[*].publishedDate").isArray
+                jsonPath("\$._embedded.stripList[*].publishedDate").value(
+                    arrayListOf("2021-10-05", "2021-01-06", "2020-10-05", "2018-10-05", "2015-08-21",
+                        "2013-07-07", "2013-07-06")
+                )
             )
             .andExpect(
-                jsonPath("\$._embedded.stripList[*].pictureUrl").exists()
+                jsonPath("\$._embedded.stripList[*].pictureUrl").value(
+                    arrayListOf("http://pdl.com/3.png", "http://xkcd.com/2.png", "http://xkcd.com/3.png", "http://pdl.com/4.png", "http://xkcd.com/1.png",
+                        "http://pdl.com/2.png", "http://pdl.com/1.png")
+                )
             )
     }
 
